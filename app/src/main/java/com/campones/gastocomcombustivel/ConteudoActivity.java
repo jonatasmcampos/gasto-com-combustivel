3e36b4989af1fe223a778bc2ae2d7e138ffdf9e0
jonatasmcampos <camposjonatas3@gmail.com>
2022-08-26 17:00:59 -0300

    criando usuario anonimo

---
+++ app/src/main/java/com/campones/gastocomcombustivel/ConteudoActivity.java
@@ -0,0 +1,14 @@
+package com.campones.gastocomcombustivel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConteudoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conteudo);
+    }
+}
--- app/src/main/java/com/campones/gastocomcombustivel/MainActivity.java
+++ app/src/main/java/com/campones/gastocomcombustivel/MainActivity.java
@@ -1,14 +1,56 @@
 package com.campones.gastocomcombustivel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.campones.gastocomcombustivel.config.ConfiguracaoFirebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        verificarUsuarioLogado();
+    }
+
+    public void acessar(View v){
+        autenticacao.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if ( task.isSuccessful() ) {
+                    chamarConteudoActivity();
+                } else {
+                    Toast.makeText(MainActivity.this, "Erro ao acessar!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        if( autenticacao.getCurrentUser() != null ){
+            chamarConteudoActivity();
+        }
+    }
+
+    public void chamarConteudoActivity(){
+        startActivity(new Intent(this, ConteudoActivity.class));
     }
 }
